5e3071d2b42f18843160d13feac2fb303833a64f
Shlok08 <89927093+Shlok08@users.noreply.github.com>
2021-09-01 20:35:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(800,400);
+  a =createSprite(400, 200, 50, 50);
+b=createSprite(200,100,30,30)
+a.shapeColor="red"
+b.shapeColor="red"
+}
+
+function draw() {
+  background(0);
+ a.x=mouseX;
+ a.y=mouseY; 
+  if(a.x-b.x<a.width/2+b.width/2  && b.x-a.x<a.width/2+b.width/2 &&  a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2){
+    a.shapeColor="green"
+    b.shapeColor="green"
+
+  }
+else{
+  a.shapeColor="red"
+b.shapeColor="red"
+}
+
+
+
+  drawSprites();
+}
